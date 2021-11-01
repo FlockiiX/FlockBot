9e54bf681d1b143fe0e16b30e95b321b23b81a34
FlockiiX <florian@berger-bayern.de>
2021-11-01 21:20:01 +0100

    updated Route and Routes

--- src/main/java/de/flockiix/flockbot/core/networking/Route.java
+++ src/main/java/de/flockiix/flockbot/core/networking/Route.java
@@ -2,14 +2,14 @@ package de.flockiix.flockbot.core.networking;
 
 import java.lang.reflect.Type;
 
-public record Route(RequestMethod requestMethod, String route, Type responseType) {
+public record Route(RequestMethod requestMethod, String url, Type responseType) {
 
     public RequestMethod getRequestMethod() {
         return requestMethod;
     }
 
-    public String getRoute() {
-        return route;
+    public String getUrl() {
+        return url;
     }
 
     public Type getResponseType() {
--- src/main/java/de/flockiix/flockbot/core/networking/Routes.java
+++ src/main/java/de/flockiix/flockbot/core/networking/Routes.java
@@ -1,5 +1,9 @@
 package de.flockiix.flockbot.core.networking;
 
+import de.flockiix.flockbot.core.model.Server;
+
 public class Routes {
-    public static final Route REGISTER_GUILD = new Route(RequestMethod.POST, "register_guild", Void.class);
+    public static final Route SEARCH_SERVER = new Route(RequestMethod.GET, "find_server", Server.class);
+    public static final Route UPDATE_SERVER = new Route(RequestMethod.POST, "update_server", Server.class);
+    public static final Route DELETE_SERVER = new Route(RequestMethod.DELETE, "delete_server", Void.class);
 }
